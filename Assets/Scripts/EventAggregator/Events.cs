b886a7386922812bc6a7293276eb1ee9c4523c19
Glebati228 <nub.azazaza@mail.ru>
2019-09-22 17:11:25 +0600

    added event agregator with his testings

---
+++ Assets/Scripts/EventAggregator/Events.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace EventAggregation
+{
+    public class SimpleEvent : IEventBase { }
+
+    public class IntEvent : IEventBase { public int Value { get; set; } } 
+
+    public class MouseLeftClickEvent : IEventBase { public Ray Point { get; set; } }
+}
---
+++ Assets/Scripts/TestSubscriber.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EventAggregation;
+
+public class TestSubscriber : MonoBehaviour
+{
+    private RaycastHit hit;
+
+    void Awake()
+    {
+        EventAggregator.Subscribe<MouseLeftClickEvent>(OnSimpleEvent);
+    }
+
+    private void OnSimpleEvent(IEventBase eventBase)
+    {
+        MouseLeftClickEvent ev = eventBase as MouseLeftClickEvent;
+
+        if(ev != null)
+        {
+            if (Physics.Raycast(ev.Point, out hit))
+            {
+                if(hit.collider.tag == "Unit")
+                {
+                    Debug.Log("I'm " + hit.collider.name);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
